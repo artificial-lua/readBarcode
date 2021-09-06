e75760dc193951df2fa0e26eeafbdffce57fb26b
artificial-lua <artificialluamail@gmail.com>
2021-09-06 10:51:49 +0900

    js-db

--- Server/main.js
+++ Server/main.js
@@ -4,6 +4,7 @@ const app = express()
 
 // 커스텀 모듈 - config decoder
 config = require('./config-decoder').open('./config.json')
+db = require('./mariadb-connector').host(config['db'])
 port = config['port']
 
 // url에 따라 분리해줍니다.
@@ -14,6 +15,9 @@ app.get('/barcode/req/', function(req, res){
 	var date = new Date();
 	res.send(respond);
 	console.log('[' + date + ']::/barcode/req:' + ip + JSON.stringify(respond));
+	db.query(`SELECT * FROM information`, function(error, result){
+		console.log(result)
+	})
 })
 
 // 마지막으로, 포트로 열어줍니다.
