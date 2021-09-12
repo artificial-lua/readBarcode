7077b522f4dbcc4b6542cff150077aceb6e0a683
artificial-lua <artificialluamail@gmail.com>
2021-09-12 20:17:58 +0900

    js

--- Server/mariadb-connector.js
+++ Server/mariadb-connector.js
@@ -16,7 +16,13 @@ function connection(json){
 }
 
 function condition(json){
-    var connection = host(json);
+    var connection = mysql.createConnection({
+        host     : json['host'],
+        port     : json['port'],
+        user     : json['user'],
+        password : json['passwd'],
+        database : json['db']
+      });
     result = connection.query(`SHOW DATABASES`, function(error, result){
         return result;
 	});
