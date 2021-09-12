f405d65f5d53034e6829c21aa7e8fab6a7aed75e
artificial-lua <artificialluamail@gmail.com>
2021-09-12 19:54:42 +0900

    db

--- Server/mariadb-connector.js
+++ Server/mariadb-connector.js
@@ -1,6 +1,10 @@
 const mysql = require('mysql');
 
 exports.host = function(json){
+    return connection(json);
+}
+
+function connection(json){
     var connection = mysql.createConnection({
         host     : json['host'],
         port     : json['port'],
