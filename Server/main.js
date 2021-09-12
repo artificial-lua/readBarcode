0a329491a391384906cc0d555f7507751390fbdb
artificial-lua <artificialluamail@gmail.com>
2021-09-12 14:05:15 +0900

    condition

--- Server/main.js
+++ Server/main.js
@@ -4,7 +4,6 @@ const app = express()
 
 // 커스텀 모듈 - config decoder
 config = require('./config-decoder').open('./config.json')
-db = require('./mariadb-connector').host(config['db'])
 port = config['port']
 
 // url에 따라 분리해줍니다.
@@ -15,9 +14,14 @@ app.get('/barcode/req/', function(req, res){
 	var date = new Date();
 	res.send(respond);
 	console.log('[' + date + ']::/barcode/req:' + ip + JSON.stringify(respond));
+	db = require('./mariadb-connector').host(config['db'])
 	db.query(`SHOW DATABASES`, function(error, result){
 		console.log(result)
 	})
+})
+
+app.get('/condition/check', function(req, res){
+	res.send(db = require('./mariadb-connector').host(config['db']))
 })
 
 // 마지막으로, 포트로 열어줍니다.
