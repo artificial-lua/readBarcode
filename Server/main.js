2f9047fa1ac2542eb7b6763ce4c2b4b54dbe4679
artificial-lua <artificialluamail@gmail.com>
2021-09-13 10:21:46 +0900

    js

--- Server/main.js
+++ Server/main.js
@@ -23,13 +23,10 @@ app.get('/barcode/req/', function(req, res){
 
 app.get('/condition/check', function(req, res){
 	if (req.query['passwd'] == config['admin']['password']){
-		var respond = mysql.condition(config['db']);
 		res.send("OK");
-		console.log(respond);
 	}
 	else{
 		console.log("Wrong Password")
-		console.log(req.query['passwd'])
 		res.send(req.query['passwd']);
 	}
 })
--- Server/mariadb-connector.js
+++ Server/mariadb-connector.js
@@ -1,4 +1,4 @@
-const mysql = require('mysql');
+const mysql = require('symc-mysql');
 
 exports.host = function(json){
     return connection(json);
@@ -13,22 +13,4 @@ function connection(json){
         database : json['db']
       });
     return connection
-}
-
-exports.condition = function (json){
-    return condition(json)
-}
-
-function condition(json){
-    var connection = mysql.createConnection({
-        host     : json['host'],
-        port     : json['port'],
-        user     : json['user'],
-        password : json['passwd'],
-        database : json['db']
-      });
-    return connection.query(`SHOW DATABASES`, function(error, result){
-        console.log(result)
-        return result;
-	});
 }
