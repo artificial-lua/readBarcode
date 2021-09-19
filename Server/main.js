7c07ca31f194686f4c4a989cbf2a20d22272d18a
artificial-lua <artificialluamail@gmail.com>
2021-09-19 23:05:36 +0900

    main

--- Server/main.js
+++ Server/main.js
@@ -22,21 +22,11 @@ function log(log){
 바코드 data - 등록, 조회
 바코드 Raw Rait - 등록, 특정 raw의 Rait 평균, 수정 (유저 개인당 Rait는 1개로 배정하므로, 유저 개별 수정이 있어야함)
 └ 바코드 data와 RawRait는 가공된 테이블에 대하여도 동일하게 적용
-
-
 */
 
-// url에 따라 분리해줍니다.
-// app.get('/barcode/req/', function(req, res){
-// 	var respond = mysql.host(db);
-//     console.log(respond);
-// 	const ip = req.headers['x-forwarded-for'] ||  req.connection.remoteAddress;
-// 	var date = new Date();
-// 	res.send(respond);
-// 	console.log('[' + date + ']::/barcode/req:' + ip + JSON.stringify(respond));
-// })
 
 // 유저
+// 유저 등록
 app.get(url['user-reg'], function(req, res) {
 	var result = mysql.user_reg(db);
 
@@ -47,7 +37,18 @@ app.get(url['user-reg'], function(req, res) {
 	log(result)
 	res.send(result)
 })
+// 유저 조회
+app.get(url['user-search'], function(req, res) {
 
+	res.send("temp user search")
+})
+// 유저 수정
+app.get(url['user-edit'], function(req, res) {
+	res.send("temp user edit")
+})
+
+// 바코드
+// 바코드 입력
 app.get(url['barcode-reg'], function(req, res){
 	var body = req.query
 	
@@ -56,23 +57,23 @@ app.get(url['barcode-reg'], function(req, res){
 	log(req.query.toString() + result.toString())
 	res.send(result)
 })
+// 바코드 조회
+app.get(url['barcode-search'], function(req, res){
+	res.send("temp barcode search")
+})
+// 바코드 평가
+app.get(url['barcode-rait'], function(req, res){
+	res.send("temp barcode rait")
+})
 
+
+// 서버 컨디션 체크
 app.get(url['condition'], function(req, res){
 	if (req.query['passwd'] == config['admin']['password']){
 		var result = mysql.condition(db);
 		log(result)
-		var comp = { Database: db['db']}
-		log(comp)
 
-		for(i = 0; i < result.lenght; i++){
-			log(result[i])
-			if(result[i] == comp)
-			{
-				res.send("ok")
-				break;
-			}
-		}
-		res.send("no ok")
+		res.send("ok")
 	}
 	else{
 		log("Wrong Password")
