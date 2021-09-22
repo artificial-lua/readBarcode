f5e8aab53c6efce9e2c02e0ff55e045e5d65b8e4
artificial-lua <artificialluamail@gmail.com>
2021-09-22 14:55:42 +0900

    js

--- Server/mariadb-parser.js
+++ Server/mariadb-parser.js
@@ -85,6 +85,7 @@ exports.user_edit = function(mysql, data, blacklist){
     }
 
     console.log("user-edit  blacklist check")
+    console.log(value)
 
     // DB 내 확인
     var value = user_search(mysql, data)
@@ -96,7 +97,6 @@ exports.user_edit = function(mysql, data, blacklist){
 
     // DB 수정
     value = mysql.query('UPDATE information SET id="' + data['edit-id'] + '" where id="' + data['id'] + '";');
-    console.log(value)
 
     if (value.length != 1){
         value = {
