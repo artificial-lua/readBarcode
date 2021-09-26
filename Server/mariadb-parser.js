60406d824d6253dda26c624b8cf42e09bf1c5fe3
artificial-lua <artificialluamail@gmail.com>
2021-09-26 23:22:30 +0900

    js

--- Server/mariadb-parser.js
+++ Server/mariadb-parser.js
@@ -161,7 +161,7 @@ exports.barcode_reg = function(mysql, data){
         
     }else{
 
-        qr = 'INSERT INTO barcode_rawdata VALUES (DEFAULT, "'; + data.barcode + '", ' + value.result.num + ', NOW(), "' + data.title + '", "");';
+        qr = 'INSERT INTO barcode_rawdata VALUES (DEFAULT, "' + data.barcode + '", ' + value.result.num + ', NOW(), "' + data.title + '", "");';
         console.log(qr)
         value = mysql.query(qr)
         if (value.error == true){
