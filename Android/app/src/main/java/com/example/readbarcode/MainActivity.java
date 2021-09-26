3607e0207edc3da30268eadb65db23fab0b56da8
artificial-lua <artificialluamail@gmail.com>
2021-09-26 10:39:58 +0900

    android

--- Android/app/src/main/java/com/example/readbarcode/MainActivity.java
+++ Android/app/src/main/java/com/example/readbarcode/MainActivity.java
@@ -2,19 +2,22 @@ package com.example.readbarcode;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Context;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-    Button btn_condition;
+import org.json.JSONException;
+import org.json.JSONObject;
 
+public class MainActivity extends AppCompatActivity {
+    // 선언부
+    Button btn_condition;
     Button btn_user_reg;
     Button btn_user_reload;
     Button btn_user_edit;
@@ -28,20 +31,121 @@ public class MainActivity extends AppCompatActivity {
     EditText ed_barcode;
     EditText ed_title;
 
-    Handler h;
-    String hand_str;
+    String id;
+    String pw;
 
-    class Parser extends Handler {
-        @Override
-        public void handleMessage(@NonNull Message msg){
-            super.handleMessage(msg);
-            log("msg is " + msg.toString());
-            log("str is " + hand_str);
-        }
+    private String getId(){
+        return this.id;
+    }
+    private void setId(String id){
+        this.id = id;
+        ed_id.setText(id);
+    }
+    private String getPw(){
+        return this.pw;
+    }
+    private void setPw(String pw){
+        this.pw = pw;
+        ed_pw.setText(pw);
     }
 
+    private JSONObject parser(String json){
+        try {
+            JSONObject response = new JSONObject(json);
+            if(response.getBoolean("error")){
+                log("is error");
+                throw new Exception();
+            }else{
+                return new JSONObject(response.getString("result"));
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+            return null;
+        }
+    }
+    
+
+    // 서버 컨디션을 체크하는 함수
+    private void condition_ckeck(){
+        log("Action - check condition");
+        class Parser extends Handler{
+            @Override
+            public void handleMessage(@NonNull Message msg){
+                super.handleMessage(msg);
+                log("msg is " + msg.toString());
+                log("str is " + Restful.getStr());
+            }
+        }
+        Parser h = new Parser();
+        Restful rest = new Restful(h);
+        rest.get(0, "");
+    }
+    // 서버에 유저를 등록하는 함수
+    private void user_reg(){
+        log("Action - user_reg");
+        class Parser extends Handler{
+            @Override
+            public void handleMessage(@NonNull Message msg){
+                super.handleMessage(msg);
+
+                String responseStr = Restful.getStr();
+
+                log("msg is " + msg.toString());
+                log("str is " + responseStr);
+
+                try {
+                    JSONObject result = parser(responseStr);
+                    setId(result.getString("id"));
+                    setPw(result.getString("password"));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        Parser h = new Parser();
+        Restful rest = new Restful(h);
+
+        String params = "?password=testpassword123";
+
+        rest.get(1, params);
+    }
+    // 유저 아이디와 비밀번호를 복구할 때 user_reload()를 사용합니다. 테스트용 함수
+    private void user_reload(){
+        setId(getId());
+        setPw(getPw());
+    }
+    // 유저 id와 pw를 변경해주는 함수
+    // id는 1회만 변경 가능함
+    private void user_edit(){
+        log("Action - user_edit");
+        class Parser extends Handler {
+            @Override
+            public void handleMessage(@NonNull Message msg){
+                super.handleMessage(msg);
+
+                String responseStr = Restful.getStr();
+
+                log("msg is " + msg.toString());
+                log("str is " + responseStr);
+            }
+        }
+        Parser h = new Parser();
+        Restful rest = new Restful(h);
+
+        String params = "?id=" + getId() + "&password=" + getPw();
+        if (getId() != ed_id.getText().toString()) {
+            params += "&edit-id=" + ed_id.getText().toString();
+        }
+        if (getPw() != ed_pw.getText().toString()) {
+            params += "&edit-password=" + ed_pw.getText().toString();
+        }
+
+        rest.get(3, params);
+    }
+
+    // 초기 시작시 뷰에 할당합니다.
     private void view_conn(){
-        log("start View Connect");
+        btn_condition = (Button) findViewById(R.id.condition_check);
         btn_user_reg = (Button) findViewById(R.id.user_reg);
         btn_user_reload = (Button) findViewById(R.id.user_reload);
         btn_user_edit = (Button) findViewById(R.id.user_edit);
@@ -49,45 +153,76 @@ public class MainActivity extends AppCompatActivity {
         btn_barcode_search = (Button) findViewById(R.id.barcode_search);
         btn_barcode_good = (Button) findViewById(R.id.barcode_rait_good);
         btn_barcode_bad = (Button) findViewById(R.id.barcode_rait_bad);
-        log("Button Connected");
 
         ed_id = (EditText) findViewById(R.id.ed_id);
         ed_pw = (EditText) findViewById(R.id.ed_pw);
         ed_barcode = (EditText) findViewById(R.id.ed_barcode);
         ed_title = (EditText) findViewById(R.id.ed_title);
-        log("Edit Text Connected");
 
 
-
-
-        btn_condition = (Button) findViewById(R.id.condition_check);
+        // 각 함수를 버튼에 할당합니다.
+        // 이후 버튼이 아니라 액션에 할당할 수 있도록 합시다.
         btn_condition.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                log("Action - click condition");
-                Restful rest = new Restful(hand_str, h);
-                rest.get(0, "");
+                condition_ckeck();
             }
         });
-        log("Condition Connected");;
-        log("View Connect Complete");
+        btn_user_reg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                user_reg();
+            }
+        });
+        btn_user_reload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                user_reload();
+            }
+        });
+        btn_user_edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                user_edit();
+            }
+        });
     }
 
+    
+    // 초기 시작시 실행
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        log("On Create");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        log("Set content view");
 
         view_conn();
+        testing(); // testing() 함수는 테스트시만 실행합시다.
+    }
 
-        Restful rest = new Restful(hand_str, h);
-        rest.test();
+    
+    /*여기부터는 테스트를 위한 부분임*/
+    TextView log_text;
+    Boolean test;
+
+    public void testing(){
+        test = true;
+        log_text = (TextView)findViewById(R.id.text_log);
+        log_text.setMovementMethod(new ScrollingMovementMethod());
+    }
+
+    public String getLogs(){
+        return log_text.getText().toString();
+    }
+
+    public void setLogs(String s){
+        log_text.setText(s);
     }
 
     public void log(String s){
-        System.out.println(s);
-        Toast.makeText(getApplicationContext(), s, Toast.LENGTH_SHORT).show();
+        if (test){
+            System.out.println(s);
+            setLogs(getLogs() + "\n" + s);
+            Toast.makeText(getApplicationContext(), s, Toast.LENGTH_SHORT).show();
+        }
     }
 }
--- Android/app/src/main/java/com/example/readbarcode/Restful.java
+++ Android/app/src/main/java/com/example/readbarcode/Restful.java
@@ -1,6 +1,8 @@
 package com.example.readbarcode;
 
+import android.content.Context;
 import android.os.Handler;
+import android.widget.Toast;
 
 import java.io.BufferedReader;
 import java.io.InputStream;
@@ -13,59 +15,79 @@ public class Restful  extends Thread{
     String[] path;
     int port;
     int c;
-    String str;
+    static String str;
     android.os.Handler h;
+    Context cont;
 
     String gp;
 
     String query;
-    public Restful(String str, Handler h){
+    public Restful(Handler h){
         this.host = Hosts.url;
         this.path = Hosts.path;
         this.port = Hosts.port;
-
-        this.str = str;
+        this.cont = cont;
         this.h = h;
     }
 
-    public void get(int c, String query) {
-        this.c = c;
+    public void get(int choose, String query) {
+        log("function get()");
+        this.c = choose;
         this.query = query;
-        this.gp = "GET";
-        this.start();
+        this.gp = "get";
+        Connection conn = new Connection();
+        conn.start();
     }
 
-    @Override
-    public void run(){
-        super.run();
-        try{
-            if (this.gp == "get"){
-                HttpURLConnection conn;
-                URL url = new URL("http", Hosts.url, Hosts.port, path[c] + query);
+    class Connection extends Thread{
 
-                conn = (HttpURLConnection) url.openConnection();
-                conn.setRequestMethod("GET");
-                conn.setDoInput(true);
+        @Override
+        public void run(){
+            super.run();
+            try{
+                if (gp == "get"){
+                    log("run - get");
+                    HttpURLConnection conn;
+                    URL url = new URL("http", "barcode.project-geek.cc", Hosts.port, path[c] + query);
 
-                InputStream is = conn.getInputStream();
-                StringBuilder sb = new StringBuilder();
-                BufferedReader br = new BufferedReader(new InputStreamReader(is, "UTF-8"));
-                String result;
-                while((result = br.readLine()) != null){
-                    sb.append(result + "\n");
+                    conn = (HttpURLConnection) url.openConnection();
+                    conn.setRequestMethod("GET");
+                    conn.setDoInput(true);
+                    log("HttpURLConnection Config Complete");
+
+                    InputStream is = conn.getInputStream();
+                    StringBuilder sb = new StringBuilder();
+                    log("Start Connection");
+                    BufferedReader br = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+                    log("End Connection");
+                    String result;
+                    log("Start Parsing Packet");
+                    while((result = br.readLine()) != null){
+                        sb.append(result + "\n");
+                    }
+                    log("End Parsing Packet");
+
+                    str = sb.toString();
+                    h.sendEmptyMessage(1);
+                    log("end get");
                 }
-
-                this.str = sb.toString();
-                this.h.sendEmptyMessage(0);
+            }catch (Exception e){
+                System.out.println(e);
             }
-        }catch (Exception e){
-            System.out.println(e);
         }
+
     }
 
 
+    public static String getStr(){
+        return str;
+    }
 
     public void test(){
         System.out.println("url:" + Hosts.url);
+    }
+
+    public void log(String s){
+        System.out.println(s);
     }
 }
