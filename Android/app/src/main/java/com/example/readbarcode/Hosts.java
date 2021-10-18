2840e6db1225d0ace1a671e89395a2b54e92ff1f
artificial-lua <artificialluamail@gmail.com>
2021-10-18 20:29:07 +0900

    android

---
+++ Android/app/src/main/java/com/example/readbarcode/Hosts.java
@@ -0,0 +1,17 @@
+package com.example.readbarcode;
+
+/* Host URL을 여기에 넣어둡니다.*/
+
+public class Hosts {
+    public static int port = 20020;
+    public static String url = "barcode.project-geek.cc";
+    public static String[] path = {
+            "/condition/check/",
+            "/user/reg/",
+            "/user/search/",
+            "/user/edit/",
+            "/barcode/reg/",
+            "/barcode/search/",
+            "/barcode/rating/"
+    };
+}
