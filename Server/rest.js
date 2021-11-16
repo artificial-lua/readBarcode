d7e55bb7b910aa70cc18aa30b824ae36c211e292
artificial-lua <artificialluamail@gmail.com>
2021-11-16 22:04:50 +0900

    rest

---
+++ Server/rest.js
@@ -0,0 +1,62 @@
+const XMLHttpRequest = require("xhr2");
+
+async function rest(url, method="GET"){
+    return new Promise((resolve, reject) => {
+        let xhr = new XMLHttpRequest(); // ajax
+        console.log("request : " + url);
+        xhr.onload = function(){
+            xhr.status===200?resolve(xhr.responseText):reject(xhr.statusText);
+        }
+    
+        // open() 메서드는 요청을 준비하는 메서드입니다. (http 메서드, 데이터를 받아올 URL 경로, 비동기 여부)
+        xhr.open(method, url, true);
+    
+        xhr.send("");
+    });
+}
+
+function getTitle(str){
+    // const title to match str between <b> and </b>
+    if(str != null){
+        let title = str.match(/<b>(.*)<\/b>/);
+        if(title != null){
+            return title[1];
+        }
+    }else{
+        return null;
+    }
+}
+
+
+//function crawlering
+function crawling(barcode, url){
+    const result = rest(url + barcode, "GET").then(function(response){
+        let title = getTitle(response);
+        if(title != null){
+            return title;
+        }else{
+            return null;
+        }
+    });
+    return result;
+}
+
+function crawling_beepscan(barcode){
+    return crawling(barcode, "https://www.beepscan.com/barcode/");
+}
+
+exports.crawling_beepscan = crawling_beepscan;
+
+function crawling_foodsafetykorea(barcode){
+    const result = ""
+
+    return result;
+}
+
+exports.crawling_foodsafetykorea = crawling_foodsafetykorea;
+
+async function main(){
+    console.log("result : " + (await crawling_foodsafetykorea("8801115111030")));
+}
+
+main();
